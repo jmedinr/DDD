e86ec574be1fe0a0df285e9ac9f92a513d6e6e59
luis <luisfelorivas@gmai.com>
2022-05-17 15:21:49 -0500

    REFACTOR: Se reorganizo las entidades y agregados en carpetas dependiendo de su respectivo agregado y tipo.

--- domain/src/main/java/co/com/sofkau/entrenamiento/curso/entities/Clientes.java
+++
@@ -1,8 +0,0 @@
-package co.com.sofkau.entrenamiento.curso.entities;
-
-import co.com.sofka.domain.generic.AggregateEvent;
-
-public class Clientes extends AggregateEvent<ClienteId> {
-     protected  Remitente remitente;
-     protected  Destinatario destinatario;
-}
--- domain/src/main/java/co/com/sofkau/entrenamiento/curso/entities/envios/values/Ubicacion.java
+++ domain/src/main/java/co/com/sofkau/entrenamiento/curso/entities/envios/values/Ubicacion.java
@@ -1,4 +1,4 @@
-package co.com.sofkau.entrenamiento.curso.entities;
+package co.com.sofkau.entrenamiento.curso.entities.envios.values;
 
 import co.com.sofka.domain.generic.ValueObject;
 
