5fce043d99c3806160bd53584b6425a2b5cf8820
luis <luisfelorivas@gmai.com>
2022-05-17 17:00:09 -0500

    ADD: se agregaron los eventos del envio: envio lovalizado y envio recibido,

---
+++ domain/src/main/java/co/com/sofkau/entrenamiento/curso/entities/envios/events/EnvioRecibido.java
@@ -0,0 +1,35 @@
+package co.com.sofkau.entrenamiento.curso.entities.envios.events;
+
+import co.com.sofka.domain.generic.DomainEvent;
+import co.com.sofkau.entrenamiento.curso.entities.envios.values.Descripcion;
+import co.com.sofkau.entrenamiento.curso.entities.envios.values.Nombre;
+
+public class EnvioRecibido extends DomainEvent {
+    private final IdPaquete idPaquete;
+    private final Nombre nombre;
+    private final Descripcion descripcion;
+    private final Entrega entrega;
+    public EnvioRecibido(IdPaquete idPaquete, Nombre nombrePaquete, Descripcion descripcionPaquete, Entrega entrega) {
+        super("co.com.sofkau.entrenamiento.curso.entities.EnvioRecibido");
+        this.nombre=nombrePaquete;
+        this.descripcion=descripcionPaquete;
+        this.idPaquete=idPaquete;
+        this.entrega=entrega;
+    }
+
+    public IdPaquete getIdPaquete() {
+        return idPaquete;
+    }
+
+    public Nombre getNombre() {
+        return nombre;
+    }
+
+    public Descripcion getDescripcion() {
+        return descripcion;
+    }
+
+    public Entrega getEntrega() {
+        return entrega;
+    }
+}
