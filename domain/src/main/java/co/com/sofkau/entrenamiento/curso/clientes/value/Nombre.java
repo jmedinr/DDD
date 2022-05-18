9504a2bd77eed88d1aa88fb593f7abcc758db774
granadalook <tiven17.jsga@gmail.com>
2022-05-18 11:54:55 -0500

    FixedDocument: se documenta  los objetos de valor

--- domain/src/main/java/co/com/sofkau/entrenamiento/curso/clientes/value/Nombre.java
+++ domain/src/main/java/co/com/sofkau/entrenamiento/curso/clientes/value/Nombre.java
@@ -3,7 +3,15 @@ package co.com.sofkau.entrenamiento.curso.clientes.value;
 import co.com.sofka.domain.generic.ValueObject;
 
 import java.util.Objects;
-
+/**
+ * Nombre es un  objeto de valor del  agregado de clientes
+ * se hacen  validaciones de campos requeridos y maxLength
+ *
+ * @Version 1.0
+ * @Author Jhon Stiven Granada Acevedo
+ * @Email ticen17.jsga@gmail.com
+ * *
+ */
 public class Nombre implements ValueObject<String> {
     private final String value;
 
